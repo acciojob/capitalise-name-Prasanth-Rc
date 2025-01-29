e54fcfad850a9bffc71c731d7275a01bad5bf0d5
Prasanth R <97664738+Prasanth-Rc@users.noreply.github.com>
2025-01-29 19:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function capitalise(){
 	var inputField = document.getElementById('fname');
 	inputField.value = inputField.value.toUpperCase(); 
 }
+
+window.onload = function() {
+  document.getElementById('fname').onblur = capitalizeName;
+};
